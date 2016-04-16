d5a3280eddeee06ca0ad8ad69009c94862aaf83a
Skylocke <mathematical.fiction@gmail.com>
2016-04-15 21:54:55 -0700

    Added files via upload

--- Project/kaggle_santander.py
+++ Project/kaggle_santander.py
@@ -4,7 +4,6 @@ Created on Thu Apr 14 18:35:47 2016
 
 @author: Rachel
 """
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,3 +12,4 @@ santander = pd.read_csv('train.csv', index_col='\'ID')
 santander['var3'].value_counts() # wtf is var3?
 santander.hist(column='var15') # this is age distribution
 santander.hist(column='imp_ent_var16_ult1', range=(1,90000)) # there are a lot of $0.00's
+# see earlier lesson on seaborn
